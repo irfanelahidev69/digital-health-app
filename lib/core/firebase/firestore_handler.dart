0de7ecb64835d4af5f5c62b320295bc3d4b7cb2f
Irfan Elahi <irfanelahi.dev@gmail.com>
2024-11-06 17:29:59 +0500

    updated firestore handler to get all data where condition is satisfied

--- lib/core/firebase/firestore_handler.dart
+++ lib/core/firebase/firestore_handler.dart
@@ -8,6 +8,7 @@ class FireStoreHandler {
 
   static const String USER = 'users';
   static const String CHECK_IN = 'check_in';
+  static const String USER_id = 'userId';
 
   factory FireStoreHandler() {
     return _instance;
@@ -21,10 +22,20 @@ class FireStoreHandler {
     _firestore = FirebaseFirestore.instance;
   }
 
-  Future<FireStoreResponse> addDocument({required String collectionName, required Map<String, dynamic> data,required String message}) async {
+  Future<FireStoreResponse> addDocument({required String collectionName, required Map<String, dynamic> data, required String message}) async {
     try {
       await _firestore.collection(collectionName).add(data);
       return FireStoreResponse(success: true, message: message);
+    } catch (e) {
+      return FireStoreResponse(success: false, message: e.toString());
+    }
+  }
+
+  Future<FireStoreResponse> getDocumentWhere({required String collectionName, required String query, required String field}) async {
+    try {
+      var data = await _firestore.collection(collectionName).where(field, isEqualTo: query).get();
+
+      return FireStoreResponse(success: true, data: [...data.docs], message: '');
     } catch (e) {
       return FireStoreResponse(success: false, message: e.toString());
     }
