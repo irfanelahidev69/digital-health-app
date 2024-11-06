1226a4078ea69897f96b37794d74c47cbc0b03c8
Irfan Elahi <irfanelahi.dev@gmail.com>
2024-11-06 17:33:40 +0500

    handled history screen state

--- lib/features/home/screen/history_page.dart
+++ lib/features/home/screen/history_page.dart
@@ -1,9 +1,13 @@
-
 import 'package:digital_health_app/core/extensions/context.dart';
+import 'package:digital_health_app/core/extensions/date_time.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 import '../../../core/component/custom_container.dart';
 import '../../../core/component/custom_list_tile.dart';
-
+import '../../../core/utilities/colors.dart';
+import '../../../core/utilities/strings.dart';
+import '../bloc/check_in_bloc/check_in_bloc.dart';
 
 class HistoryPage extends StatefulWidget {
   const HistoryPage({
@@ -15,9 +19,13 @@ class HistoryPage extends StatefulWidget {
 }
 
 class _HistoryPageState extends State<HistoryPage> {
-  final TextEditingController nameController = TextEditingController();
-  final GlobalKey<FormState> _key = GlobalKey<FormState>();
-  bool isSwitchOn = false;
+  final CheckInBloc checkInBloc = CheckInBloc();
+
+  @override
+  void initState() {
+    checkInBloc.add(GetCheckInHistoryEvent());
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +35,11 @@ class _HistoryPageState extends State<HistoryPage> {
         return true;
       },
       child: Padding(
-        padding: const EdgeInsets.only(left: 20.0,right: 20.0,top: 20.0,),
+        padding: const EdgeInsets.only(
+          left: 20.0,
+          right: 20.0,
+          top: 20.0,
+        ),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.center,
@@ -43,36 +55,62 @@ class _HistoryPageState extends State<HistoryPage> {
               ),
             ),
             Expanded(
-              child: ListView.builder(
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  return const Padding(
-                    padding: EdgeInsets.only(bottom: 20.0),
-                    child: CustomContainer(
-                      children:  [
-                        CustomListTile(
+              child: BlocConsumer<CheckInBloc, CheckInState>(
+                bloc: checkInBloc,
+                listener: (context, state) {},
+                builder: (context, state) {
+                  if (state is CheckInLoading) {
+                    return const Center(
+                      child: SizedBox(
+                          width: 30,
+                          height: 30,
+                          child: CircularProgressIndicator(
+                            color: AppColors.kOrangeColor,
+                          )),
+                    );
+                  } else if (state is CheckInUnsuccessful) {
+                    return Center(
+                      child: Text(
+                        state.message,
+                        style: context.titleMedium,
+                      ),
+                    );
+                  } else if (state is NoInternet) {
+                    return Center(
+                      child: Text(
+                        Strings.noInternetConnection,
+                        style: context.titleMedium,
+                      ),
+                    );
+                  }
 
-                          labelFirst: "Item ID:",
-                          valueFirst: "1331139979",
-                          isBold: true,
-                        ),
-                        CustomListTile(
+                  if (state is CheckInHistory) {
+                    return state.checkInList.isEmpty
+                        ? Center(
+                            child: Text(
+                              Strings.noRecordFound,
+                              style: context.titleMedium,
+                            ),
+                          )
+                        : ListView.builder(
+                            itemCount: state.checkInList.length,
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                padding: const EdgeInsets.only(bottom: 20.0),
+                                child: CustomContainer(
+                                  children: [
+                                    CustomListTile(
+                                      labelFirst: "Date & time :",
+                                      valueFirst: state.checkInList[index].checkInTime.toReadableDateTime(),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                          );
+                  }
 
-                          labelFirst: "Received Date:",
-                          valueFirst: "2022-06-30, 01:00:00",
-
-                        ),
-                        CustomListTile(
-                          labelFirst: "Segment:",
-                          valueFirst: "Standard",
-                        ),
-                        CustomListTile(
-                          labelFirst: "Status:",
-                          valueFirst: "Order Received & Ready to Pick",
-                        ),
-                      ],
-                    ),
-                  );
+                  return const SizedBox();
                 },
               ),
             ),
