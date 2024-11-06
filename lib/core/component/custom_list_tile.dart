c4892d85be65d6a3105c238ad1da43d640fbdf64
Irfan Elahi <irfanelahi.dev@gmail.com>
2024-11-06 20:17:01 +0500

    updated list tile

--- lib/core/component/custom_list_tile.dart
+++ lib/core/component/custom_list_tile.dart
@@ -1,14 +1,16 @@
+import 'package:digital_health_app/core/component/app_custom_buttons.dart';
+import 'package:digital_health_app/core/extensions/context.dart';
 import 'package:flutter/material.dart';
 
 class CustomListTile extends StatelessWidget {
-  final String labelFirst;
-  final String valueFirst;
+  final String label;
+  final String value;
   final bool isBold;
 
   const CustomListTile({
     super.key,
-    required this.labelFirst,
-    required this.valueFirst,
+    required this.label,
+    required this.value,
     this.isBold = false,
   });
 
@@ -20,11 +22,12 @@ class CustomListTile extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Text(
-            labelFirst,
+            label,
             style: TextStyle(fontWeight: isBold ? FontWeight.w700 : FontWeight.w500, fontSize: 16),
           ),
           SizedBox(
-            child: Text((valueFirst.isNotEmpty) ? valueFirst : "-", style: TextStyle(fontWeight: isBold ? FontWeight.w700 : FontWeight.w400, fontSize: 14)),
+            width: context.width * 0.4,
+            child: Text((value.isNotEmpty) ? value : "-", maxLines: 4,overflow: TextOverflow.ellipsis,style: TextStyle(fontWeight: isBold ? FontWeight.w700 : FontWeight.w400, fontSize: 14)),
           ),
         ],
       ),
