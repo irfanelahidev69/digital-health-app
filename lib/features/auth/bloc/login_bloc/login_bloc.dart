d3b4c7b7f97e2b2b5a0d851733a1cb5edd26a77f
Irfan Elahi <irfanelahi.dev@gmail.com>
2024-11-06 20:16:39 +0500

    invalid credential exception handled

--- lib/features/auth/bloc/login_bloc/login_bloc.dart
+++ lib/features/auth/bloc/login_bloc/login_bloc.dart
@@ -1,25 +1,18 @@
-import 'dart:convert';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../core/model/user.dart';
 import '../../../../core/network/network_connection.dart';
 
-
 part 'login_event.dart';
-
 part 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
-
-
   LoginBloc() : super(LoginInitial()) {
     FirebaseAuth auth = FirebaseAuth.instance;
     on<UserLoginEvent>((event, emit) async {
       emit(LoginLoadingState());
       try {
-
         bool isConnection = await NetworkConnection().checkConnection();
         if (!isConnection) {
           emit(NoInternetState());
@@ -28,15 +21,15 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
         UserCredential userCredential = await auth.signInWithEmailAndPassword(email: event.email, password: event.password);
 
         if (userCredential.user != null) {
-
           emit(LoginSuccessState());
-
         } else {
           emit(LoginFailedState(message: "Something went wrong"));
         }
       } on FirebaseAuthException catch (e) {
         if (e.code == 'user-not-found') {
           emit(LoginFailedState(message: "No user found for that email"));
+        } else if (e.code == 'invalid-credential') {
+          emit(LoginFailedState(message: "Invalid Credentials"));
         } else if (e.code == 'wrong-password') {
           emit(LoginFailedState(message: "Email or password is incorrect"));
         }
@@ -44,6 +37,5 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
         emit(LoginFailedState(message: "Something went wrong"));
       }
     });
-
   }
 }
