b950622f94b454b3209c87d18d9cd2a2ccfddc88
Irfan Elahi <irfanelahi.dev@gmail.com>
2024-11-06 14:32:10 +0500

    handled login states

--- lib/features/auth/pages/login_screen.dart
+++ lib/features/auth/pages/login_screen.dart
@@ -1,12 +1,16 @@
 import 'package:digital_health_app/core/extensions/context.dart';
 import 'package:digital_health_app/features/auth/pages/signup_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../core/component/app_custom_buttons.dart';
+import '../../../core/component/custom_dialog.dart';
 import '../../../core/component/custom_form_text_field.dart';
 import '../../../core/utilities/colors.dart';
 import '../../../core/utilities/strings.dart';
 import '../../../core/utilities/validators.dart';
+import '../bloc/login_bloc/login_bloc.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({
@@ -23,12 +27,7 @@ class _LoginScreenState extends State<LoginScreen> with Validators {
   bool isVisible = true;
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-
-  @override
-  void initState() {
-    init();
-    super.initState();
-  }
+  final LoginBloc bloc = LoginBloc();
 
   @override
   Widget build(BuildContext context) {
@@ -117,14 +116,79 @@ class _LoginScreenState extends State<LoginScreen> with Validators {
                               ],
                             ),
                           ),
-                          AppButton(
-                            isProcessing: false,
-                            onPressed: () async {},
-                            child: Text(
-                              Strings.login,
-                              style: context.labelLarge.copyWith(fontWeight: FontWeight.w600, color: Colors.white),
-                            ),
-                          )
+                          BlocConsumer<LoginBloc, LoginState>(
+                            bloc: bloc,
+                            listener: (context, state) {
+                              if (state is LoginSuccessState) {}
+                              if (state is LoginGetUserState) {}
+                              if (state is LoginFailedState) {
+                                context.showAppDialog(
+                                  CustomDialog(
+                                    height: 40,
+                                    content: Strings.error,
+                                    description: state.errorCode != null ? "${state.message}" : state.message,
+                                    actions: [
+                                      CupertinoDialogAction(
+                                        child: Text(
+                                          Strings.ok,
+                                          style: context.titleSmall,
+                                        ),
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              }
+                              if (state is NoInternetState) {
+                                context.showAppDialog(
+                                  CustomDialog(
+                                    height: 40,
+                                    content: Strings.message,
+                                    description: Strings.noInternetConnection,
+                                    // icon: Assets.icons.closeIcon.path,
+                                    actions: [
+                                      CupertinoDialogAction(
+                                        child: Text(
+                                          Strings.retry,
+                                          style: context.titleSmall,
+                                        ),
+                                        onPressed: () {
+                                          bloc.add(
+                                            UserLoginEvent(
+                                              email: _emailController.text.trim(),
+                                              password: _passwordController.text.trim(),
+                                            ),
+                                          );
+                                          Navigator.pop(context);
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              }
+                            },
+                            builder: (context, state) {
+                              return AppButton(
+                                isProcessing: state is LoginLoadingState ? true : null,
+                                onPressed: () {
+                                  if (_formKey.currentState!.validate()) {
+                                    bloc.add(
+                                      UserLoginEvent(
+                                        email: _emailController.text.trim(),
+                                        password: _passwordController.text.trim(),
+                                      ),
+                                    );
+                                  }
+                                },
+                                child: Text(
+                                  Strings.login,
+                                  style: context.labelLarge.copyWith(fontWeight: FontWeight.w600, color: Colors.white),
+                                ),
+                              );
+                            },
+                          ),
                         ],
                       ),
                     ),
@@ -164,6 +228,4 @@ class _LoginScreenState extends State<LoginScreen> with Validators {
       ),
     );
   }
-
-  init() {}
 }
